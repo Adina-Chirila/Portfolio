ccee1619fd1b6ec8edfe6bc4709fbaf87ba38311
Adina Chirila <adina.ch55@gmail.com>
2021-08-25 20:22:28 +0300

    Finish footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { FooterSection } from "./Footer.styled";
 
 const Footer = () => {
-	return (
-		<div style={{ backgroundColor: "#141628", color: "white" }}>Footer</div>
-	);
+    return (
+        <FooterSection>
+            <span>&copy;</span> 2021 Designed &amp; Built by
+            <a
+                href="https://github.com/Adina-Chirila"
+                target="_blank"
+                rel="noreferrer"
+            >
+                Adina Chirila
+            </a>
+        </FooterSection>
+    );
 };
 
 export default Footer;
---
+++ src/components/Footer/Footer.styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { colors } from "../../shared/colors";
+
+export const FooterSection = styled.footer`
+    font-size: 12px;
+    background-color: ${colors.deepDarkBlue};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1em 0;
+
+    a {
+        margin-left: 0.4em;
+        color: ${colors.white};
+    }
+
+    span {
+        margin-right: 0.5em;
+        color: ${colors.primaryPink};
+    }
+`;
