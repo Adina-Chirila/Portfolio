d38e256764a2b2aa868331aaacb74c97ee5bd677
Adina Chirila <adina.ch55@gmail.com>
2021-09-19 19:09:58 +0300

    Update about text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,22 +12,19 @@ const About = () => {
                     <h1 className="about-title">About me</h1>
                     <p>
                         My interest in Front-end Development started back in
-                        2019. After graduated my Master’s Degree and changed a
-                        few jobs in fields not related with development, I felt
-                        my potential was not fully used on the fields I worked.
+                        2019, when I started looking for fields that I thought
+                        could offer me the possibility of continuous learning
+                        and the opportunity to grow professionally.
                     </p>
                     <p>
-                        Then I realized my professional career should change, so
-                        I started looking for fields that I thought could offer
-                        me the motivation and satisfaction I needed and the
-                        opportunities to grow professionally on the long term.
                         That's how I discovered Front-end development, a
                         continuous growing field that offers me the challenges I
-                        need in order to develop myself and grow professionally.
-                        So I started reading, learning and building projects,
-                        and now, I feel pretty confident that I can be
-                        productive in a business environment and turn my passion
-                        into a career.
+                        need in order to develop myself. So I started reading,
+                        learning and building projects and in 2020 I succeeded
+                        to obtain the Web Development certification from the
+                        Informal School od IT. Currently, I feel pretty
+                        confident that I can be productive in a business
+                        environment and turn my passion into a career.
                     </p>
                     <p>
                         Here are the technologies I've been working with so far:
